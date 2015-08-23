cf7b2235271f654818e3e3f688c9ad0387f053aa
daniel <abrahamdaniel92@gmail.com>
2015-08-23 18:08:40 +0530

    changed to babelify for transforming

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module RailsDevisePundit
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.browserify_rails.commandline_options = "--transform reactify --extension=\".js.jsx\""
+    config.browserify_rails.commandline_options = "--transform babelify --extension=\".js.jsx\""
     config.react.variant = :production
     config.react.addons = true
   end
