c8d493199b7173f9f7da29c233013757dd94593e
daniel <abrahamdaniel92@gmail.com>
2015-08-25 09:33:18 +0530

    Question is invalid without description

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 describe Question, type: :model do
   describe '#valid?' do
-    it 'should be a valid question' do
+    it 'is a a valid question' do
       question = FactoryGirl.build(:question)
       expect(question).to be_valid
+    end
+    it 'is not a valid question without description' do
+      question = FactoryGirl.build(:question, description: nil)
+      expect(question).to_not be_valid
     end
   end
 end
