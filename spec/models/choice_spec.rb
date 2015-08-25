541c944ee422fbfc0cfe80e85c48cfda1843df16
daniel <abrahamdaniel92@gmail.com>
2015-08-25 11:33:51 +0530

    added pending specs to questions

--- spec/models/choice_spec.rb
+++ spec/models/choice_spec.rb
@@ -1,24 +1,29 @@
 describe Choice do
+
+  it { should belong_to(:question) }
+
   describe '#valid?' do
     it 'is invalid without text' do
       choice = FactoryGirl.build(:choice, :text => nil)
       expect(choice).to_not be_valid
     end
+
     it 'is valid with text' do
       choice = FactoryGirl.build(:choice)
       expect(choice).to be_valid
     end
   end
+
   describe '#correct?' do
-    it 'is a right choice when correct if true' do
+    it 'is a right choice when correct is true' do
       choice = FactoryGirl.build(:choice, correct: true)
       expect(choice.correct).to be true
     end
-    it 'is not a right choice when correct if false' do
+
+    it 'is not a right choice when correct is false' do
       choice = FactoryGirl.build(:choice, correct: false)
       expect(choice.correct).to be false
     end
   end
 
-  it { should belong_to(:question) }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -14,6 +14,10 @@ describe Question, type: :model do
       question = FactoryGirl.build(:question, description: nil)
       expect(question).to_not be_valid
     end
+
+    it 'is valid if it has four choices'
+    it 'is not valid if it has less than four choices'
+    it 'is not valid if it has more than three choices'
   end
 
 end
