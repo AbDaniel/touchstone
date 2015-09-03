974b09754e24da03f4fbc51980488cc2e24d451d
daniel <abrahamdaniel92@gmail.com>
2015-09-03 10:27:33 +0530

    Added a context to question spec to validate question with choices

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,20 +2,23 @@ require 'rails_helper'
 
 describe Question, type: :model do
 
-  let(:question) { FactoryGirl.build(:question) }
+  let(:question) { FactoryGirl.create(:question_with_choices) }
 
   subject { question }
-
-  it { should have_many(:choices) }
-
-  it { should be_valid }
 
   it 'is not a valid question without description' do
     question = FactoryGirl.build(:question, description: nil)
     expect(question).to_not be_valid
   end
 
-  it 'is valid if it has four choices'
-  it 'is not valid if it has less than four choices'
-  it 'is not valid if it has more than three choices'
+  context 'question with choices' do
+
+    it { should have_many(:choices) }
+
+    it 'should have four choices' do
+      expect(question.choices.count).to eq(4)
+    end
+    
+  end
+
 end
