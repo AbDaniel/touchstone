9c974961652acac11167f96ed31239e7a19c1d2c
daniel <abrahamdaniel92@gmail.com>
2015-09-04 09:50:33 +0530

    Generated Exam model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903151350) do
+ActiveRecord::Schema.define(version: 20150904041941) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20150903151350) do
   end
 
   add_index "choices", ["question_id"], name: "index_choices_on_question_id"
+
+  create_table "exams", force: :cascade do |t|
+    t.string   "code"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "description"
---
+++ spec/factories/exams.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :exam do
+    code "MyString"
+name "MyString"
+  end
+
+end
