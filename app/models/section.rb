fe8f4752b888a9d1225d9f9fff19075c36505d00
daniel <abrahamdaniel92@gmail.com>
2015-09-05 11:43:12 +0530

    Section is invalid without no of questions

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,5 @@ class Section < ActiveRecord::Base
   belongs_to :category
 
   validates_presence_of :category
+  validates_presence_of :no_of_questions
 end
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -8,9 +8,18 @@ describe Section do
 
   it { should be_valid }
 
-  it 'is not valid if category is not present' do
+  it 'is invalid if category is not present' do
     section = FactoryGirl.build(:section, category: nil)
     expect(section).to_not be_valid
   end
+
+  it 'is invalid without no of questions' do
+    section = FactoryGirl.build(:section, no_of_questions: nil)
+    expect(section).to_not be_valid
+  end
+
+  it 'has more positive no of questions'
+
+  it 'does not have negative no of questions '
 
 end
