aac6ae19b19ce2dd17fb5a164b47f7dd2065d386
daniel <abrahamdaniel92@gmail.com>
2015-09-05 11:23:30 +0530

    Section is invalid without a category

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,5 @@
 class Section < ActiveRecord::Base
   belongs_to :category
+
+  validates_presence_of :category
 end
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -8,4 +8,9 @@ describe Section do
 
   it { should be_valid }
 
+  it 'is not valid if category is not present' do
+    section = FactoryGirl.build(:section, category: nil)
+    expect(section).to_not be_valid
+  end
+
 end
