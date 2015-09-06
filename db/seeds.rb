62431bd3eb605a763b1c36221b03ba084bbfd376
daniel <abrahamdaniel92@gmail.com>
2015-09-06 11:34:56 +0530

    Added database seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,56 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+#Categories
+category_list = [
+    'Probability', 'Time and Distance', 'Number Theorem', 'Permutations'
+]
+
+category_list.each do |category_name|
+  Category.create(name: category_name)
+end
+
+#Questions
+category_probability = Category.find_by_name 'Probability'
+category_time = Category.find_by_name 'Time and Distance'
+category_number = Category.find_by_name 'Number Theorem'
+category_permutation = Category.find_by_name 'Permutations'
+
+(1..5).each do |index|
+  question = Question.create(description: "Prob Question #{index}", categories: [category_probability])
+  (1..3).each do |choice_no|
+    Choice.create(text: "Choice #{choice_no}", correct: false, question: question)
+  end
+  Choice.create(text: 'Choice 4', correct: true, question: question)
+end
+(6..10).each do |index|
+  question = Question.create(description: "Time Question #{index}", categories: [category_time])
+  (1..3).each do |choice_no|
+    Choice.create(text: "Choice #{choice_no}", correct: false, question: question)
+  end
+  Choice.create(text: 'Choice 4', correct: true, question: question)
+end
+(11..15).each do |index|
+  question = Question.create(description: "Number Question #{index}", categories: [category_number])
+  (1..3).each do |choice_no|
+    Choice.create(text: "Choice #{choice_no}", correct: false, question: question)
+  end
+  Choice.create(text: 'Choice 4', correct: true, question: question)
+end
+(16..20).each do |index|
+  question = Question.create(description: "Perm Question #{index}", categories: [category_permutation])
+  (1..3).each do |choice_no|
+    Choice.create(text: "Choice #{choice_no}", correct: false, question: question)
+  end
+  Choice.create(text: 'Choice 4', correct: true, question: question)
+end
+
+exam = Exam.create(code: 'CS-101', name: 'Introduction to Programming')
+exam_configuration = ExamConfiguration.create(exam: exam, duration: '2 hours')
+sections = [[category_probability, 3], [category_time, 3], [category_number, 3], [category_permutation, 3]]
+
+sections.each do |category, no_of_questions|
+  Section.create(category: category, no_of_questions: no_of_questions, exam_configuration: exam_configuration)
+end
+
