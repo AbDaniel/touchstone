47735b902427f529e5145775f091b7b69562bf61
daniel <abrahamdaniel92@gmail.com>
2015-09-07 20:33:44 +0530

    Configured rails generator to generate controller spec during scaffold

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module RailsDevisePundit
                        view_specs: false,
                        helper_specs: false,
                        routing_specs: false,
-                       controller_specs: false,
+                       controller_specs: true,
                        request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
