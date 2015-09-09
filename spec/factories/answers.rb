5e457ed0fd7f3d307f2376502e2dbb04f7aebff3
daniel <abrahamdaniel92@gmail.com>
2015-09-09 06:34:02 +0530

    Answers factory now can create right answers and wrong answers

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -12,11 +12,22 @@
 #
 
 FactoryGirl.define do
-  factory :answer do
-    exam nil
-user nil
-question nil
-answer "MyString"
+  factory :wrong_answer do
+    exam { create(:exam) }
+    user { create(:user) }
+    question { create(:question,
+                      choices: [create(:choice, text: 'Choice 1'), create(:choice, text: 'Choice 2'),
+                                create(:choice, text: 'Choice 3', correct: true), create(:choice, text: 'Choice 4')])
+    }
+    answer 'Choice 3'
   end
-
+  factory :correct_correct do
+    exam { create(:exam) }
+    user { create(:user) }
+    question { create(:question,
+                      choices: [create(:choice, text: 'Choice 1'), create(:choice, text: 'Choice 2'),
+                                create(:choice, text: 'Choice 3', correct: true), create(:choice, text: 'Choice 4')])
+    }
+    answer 'Choice 2'
+  end
 end
